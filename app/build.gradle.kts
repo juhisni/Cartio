ee1e4a0ce1c17538f639e4dea19de16b79b2792e
Juha-Matti Niiranen <juha-matti.niiranen@tuni.fi>
2026-08-17 20:04:13 +0300

    Bump app version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "fi.cartio"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
